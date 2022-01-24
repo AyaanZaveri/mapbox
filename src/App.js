8b1d8ee5df6546a26b14694277d0dbb27b5ed979
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-01-24 14:55:08 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import {
+  RulerControl,
+  StylesControl,
+  CompassControl,
+  ZoomControl,
+} from "mapbox-gl-controls";
+import "mapbox-gl-controls/lib/controls.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+
+class App extends React.Component {
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-73.985664, 40.748514],
+      zoom: 12,
+    });
+
+    const directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: "metric",
+      profile: "mapbox/driving",
+    });
+
+    // Directions
+    map.addControl(directions, "top-left");
+
+    // Ruler
+    map.addControl(new RulerControl(), "top-right");
+    map.on("ruler.on", () => console.log("ruler: on"));
+    map.on("ruler.off", () => console.log("ruler: off"));
+
+    // Styles
+    map.addControl(
+      new StylesControl({
+        styles: [
+          {
+            label: "Streets",
+            styleName: "Mapbox Streets",
+            styleUrl: "mapbox://styles/mapbox/streets-v11",
+          },
+          {
+            label: "Satellite",
+            styleName: "Satellite",
+            styleUrl: "mapbox://styles/mapbox/satellite-streets-v11",
+          },
+        ],
+        onChange: (style) => console.log(style),
+      }),
+      "bottom-left"
+    );
+
+    // Compass
+    map.addControl(new CompassControl(), "top-right");
+
+    // Zoom
+    map.addControl(new ZoomControl(), "top-right");
+  }
+  render() {
+    return <div className="mapWrapper" id="map" />;
+  }
 }
-
 export default App;
